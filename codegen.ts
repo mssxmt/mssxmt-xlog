992686f0b56db6e13bec5b1d2e458951ff4669d2
{Ximoto} <{orela.rec@gmail.com}>
2023-09-21 23:28:01 +0900

    codegen

---
+++ codegen.ts
@@ -0,0 +1,42 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: [
+    {
+      [process.env.NEXT_PUBLIC_HASURA_GRAPHQL_URL!]: {
+        headers: {
+          // If you have an admin secret set
+          'x-hasura-admin-secret': process.env.HASURA_GRAPHQL_ADMIN_SECRET!,
+        },
+      },
+    },
+  ],
+  config: {
+    skipTypename: true,
+    enumsAsTypes: true,
+    scalars: {
+      numeric: 'number',
+    },
+  },
+  documents: 'lib/service/queries.graphql',
+  generates: {
+    'lib/gql/gql.ts': {
+      // preset: 'client',
+      // config: {},
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-react-apollo',
+      ],
+    },
+  },
+};
+//どっかで読んだgeneratesの項目
+// generates: {
+// 'lib/gql/': {
+//   preset: 'client',
+//   config: {},
+//   plugins: [],
+// },}
+export default config;
