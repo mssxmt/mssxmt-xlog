506458c9701600e901181ca6429f5ebe6e707a36
mssxmt <orela.rec@gmail.com>
2023-10-01 00:55:29 +0900

    メタ情報と各種プロバイダー追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,77 @@
 import { Metadata } from 'next';
 import '../styles/globals.css';
 import { UserProvider } from '@auth0/nextjs-auth0/client';
+import Navbar from '../components/navbar';
+import { NextThemeProvider } from '../providers/NextThemeProvider';
+import { ApolloWrapper } from '../lib/apollo/apollo-wrapper';
+import ReactToalsyProvider from '../providers/ReactToalsyProvider';
+import 'react-toastify/dist/ReactToastify.css';
+
+const siteName = 'MSSXMTXLOG';
+const description = 'MaSaShi XiMoTo x Log';
+const url = 'https://mssxmt-xlog.netlify.app/';
 
 export const metadata: Metadata = {
-  title: 'Home',
-  description: 'WELCOME2MSSXMTBLOG',
+  // Netlifyなので指定が必要 https://docs.netlify.com/configure-builds/environment-variables/
+  metadataBase: new URL(process.env.MY_URL ?? 'http://localhost:3000'),
+  description,
+  title: {
+    default: siteName,
+    /** `next-seo`の`titleTemplate`に相当する機能 */
+    template: `%s - ${siteName}`,
+  },
+  openGraph: {
+    title: siteName,
+    description,
+    url,
+    siteName,
+    locale: 'ja_JP',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteName,
+    description,
+    site: '@marip13',
+    creator: '@marpi13',
+  },
+  verification: {
+    google: 'サーチコンソール',
+  },
+  alternates: {
+    canonical: url,
+  },
 };
 
-export default async function RootLayout({
-  children,
-}: {
+export default function RootLayout(props: {
   children: React.ReactNode;
+  modal: React.ReactNode;
 }) {
   return (
-    <html lang='ja' data-theme='retro'>
+    <html lang='ja' suppressHydrationWarning>
       <body>
-        <UserProvider>{children}</UserProvider>
+        <UserProvider>
+          <ApolloWrapper>
+            <NextThemeProvider>
+              <ReactToalsyProvider>
+                <Navbar />
+                <div
+                  className='min-h-screen'
+                  style={{
+                    // background: 'url(/images/bg2.png) center / cover',
+                    background:
+                      'conic-gradient(from 20deg, rgba(223, 168, 168, 0.5) 18deg, rgba(193, 199, 197, 0.5) 68deg, rgba(108, 199, 221, 0.5) 151deg, rgba(177, 151, 223, 0.5) 263deg, rgba(122, 197, 216, 0.5) 295deg, rgba(223, 168, 168, 0.5) 360deg), url(/images/bg2.png) ',
+                    width: '100%',
+                    backgroundRepeat: 'repeat',
+                  }}
+                >
+                  {props.children}
+                  {props.modal}
+                </div>
+              </ReactToalsyProvider>
+            </NextThemeProvider>
+          </ApolloWrapper>
+        </UserProvider>
       </body>
     </html>
   );
