1999e4905226d4f7149bae36363e6a0b89fb8ca3
mssxmt <orela.rec@gmail.com>
2023-10-01 00:51:46 +0900

    カバーイメージコンポーネント

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -1,33 +1,22 @@
-import cn from 'classnames'
-import Link from 'next/link'
+import cn from 'classnames';
+// import Link from 'next/link';
 
 type Props = {
-  title: string
-  src: string
-  slug?: string
-}
+  title: string;
+  src: string;
+  slug?: string;
+};
 
 const CoverImage = ({ title, src, slug }: Props) => {
-  const image = (
-    <img
-      src={src}
-      alt={`Cover Image for ${title}`}
-      className={cn('shadow-sm', {
-        'hover:shadow-lg transition-shadow duration-200': slug,
-      })}
-    />
-  )
+  const image = <img src={src} alt={`Cover Image for ${title}`} />;
   return (
-    <div className="sm:mx-0">
-      {slug ? (
-        <Link as={`/posts/${slug}`} href="/posts/[slug]">
-          <a aria-label={title}>{image}</a>
-        </Link>
-      ) : (
-        image
-      )}
-    </div>
-  )
-}
+    <section
+      className='flex justify-center items-center'
+      style={{ minHeight: 'calc(100vh - 0px)' }}
+    >
+      {image}
+    </section>
+  );
+};
 
-export default CoverImage
+export default CoverImage;
