1c8bf303279462bc7c9a86283fc66986f7b7eee8
mssxmt <orela.rec@gmail.com>
2023-10-04 00:34:07 +0900

    スタイルをlayout.tsxに移動

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,10 +2,7 @@ import About from '../../components/about';
 
 export default async function Page() {
   return (
-    <div
-      className='hero-overlay bg-opacity-30'
-      style={{ minHeight: ' calc(100vh - 32px)' }}
-    >
+    <div className='hero-overlay bg-opacity-30'>
       <About name='Masashi Ximoto' picture='/images/logo2.png' />
     </div>
   );
