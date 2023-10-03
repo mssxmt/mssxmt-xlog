e80148b05aac8f8617e028c9cdc6d96bea8e02f1
mssxmt <orela.rec@gmail.com>
2023-10-04 00:35:28 +0900

    ソーシャルリンクコンポーネント

---
+++ components/socialLinks.tsx
@@ -0,0 +1,187 @@
+export const SocialLinks = () => {
+  return (
+    <div className='flex justify-center gap-2'>
+      {/* github */}
+      <a href='https://github.com/mssxmt'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='36'
+          height='36'
+          viewBox='0 0 24 24'
+          className='fill-primary'
+          strokeWidth='2'
+          //   className='ai ai-GithubFill'
+        >
+          <g clip-path='url(#clip0_100_4)'>
+            <path
+              fillRule='evenodd'
+              clipRule='evenodd'
+              d='M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12z'
+            />
+          </g>
+          <defs>
+            <clipPath id='clip0_100_4'>
+              <rect width='24' height='24' />
+            </clipPath>
+          </defs>
+        </svg>
+      </a>
+      {/* twitter */}
+      <a href='http://twitter.com/marpi13'>
+        <svg
+          fill='#DC7633'
+          xmlns='http://www.w3.org/2000/svg'
+          width='36'
+          height='36'
+          zoomAndPan='magnify'
+          viewBox='0 0 375 374.9999'
+          version='1.0'
+        >
+          <defs>
+            <path
+              d='M 11.972656 11.972656 L 359.222656 11.972656 L 359.222656 359.222656 L 11.972656 359.222656 Z M 11.972656 11.972656 '
+              className='fill-primary'
+            ></path>
+          </defs>
+          <g>
+            <path
+              d='M 185.597656 359.222656 C 89.675781 359.222656 11.972656 280.984375 11.972656 185.597656 C 11.972656 90.210938 89.675781 11.972656 185.597656 11.972656 C 281.519531 11.972656 359.222656 89.675781 359.222656 185.597656 C 359.222656 281.519531 280.984375 359.222656 185.597656 359.222656 Z M 185.597656 22.691406 C 95.570312 22.691406 22.691406 95.570312 22.691406 185.597656 C 22.691406 275.625 95.570312 348.503906 185.597656 348.503906 C 275.625 348.503906 348.503906 275.625 348.503906 185.597656 C 348.503906 95.570312 275.089844 22.691406 185.597656 22.691406 Z M 185.597656 22.691406 '
+              fillOpacity='1'
+              fillRule='nonzero'
+              className='fill-primary'
+            ></path>
+          </g>
+          <g transform='translate(85, 75)'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 24 24'
+              version='1.1'
+              height='205'
+              width='205'
+            >
+              <path
+                d='M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z'
+                className='fill-primary'
+              ></path>
+            </svg>
+          </g>
+        </svg>
+      </a>
+      {/* bandcamp */}
+      <a href='https://masashiximoto.bandcamp.com/'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 512 512'
+          id='IconChangeColor'
+          height='36'
+          width='36'
+        >
+          <path
+            d='M256,8C119,8,8,119,8,256S119,504,256,504,504,393,504,256,393,8,256,8Zm48.2,326.1h-181L207.9,178h181Z'
+            id='mainIconPathAttribute'
+            className='fill-primary'
+          ></path>
+        </svg>
+      </a>
+      {/* youtube */}
+      <a href='https://www.youtube.com/@ORELArecords'>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='36'
+          height='36'
+          viewBox='0 0 24 24'
+          className='fill-primary'
+        >
+          <path d='M16.23 7.102c-2.002-.136-6.462-.135-8.461 0-2.165.148-2.419 1.456-2.436 4.898.017 3.436.27 4.75 2.437 4.898 1.999.135 6.459.136 8.461 0 2.165-.148 2.42-1.457 2.437-4.898-.018-3.436-.271-4.75-2.438-4.898zm-6.23 7.12v-4.444l4.778 2.218-4.778 2.226zm2-12.222c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12z' />
+        </svg>
+      </a>
+      <a href='https://audiostock.jp/artists/28016'>
+        <svg
+          id='changeColor'
+          className='fill-primary'
+          xmlns='http://www.w3.org/2000/svg'
+          width='36'
+          height='36'
+          zoomAndPan='magnify'
+          viewBox='0 0 375 374.9999'
+          version='1.0'
+        >
+          <defs>
+            <path
+              id='pathAttribute'
+              d='M 11.972656 11.972656 L 359.222656 11.972656 L 359.222656 359.222656 L 11.972656 359.222656 Z M 11.972656 11.972656 '
+            ></path>
+          </defs>
+          <g>
+            <path
+              id='pathAttribute'
+              d='M 185.597656 359.222656 C 89.675781 359.222656 11.972656 280.984375 11.972656 185.597656 C 11.972656 90.210938 89.675781 11.972656 185.597656 11.972656 C 281.519531 11.972656 359.222656 89.675781 359.222656 185.597656 C 359.222656 281.519531 280.984375 359.222656 185.597656 359.222656 Z M 185.597656 22.691406 C 95.570312 22.691406 22.691406 95.570312 22.691406 185.597656 C 22.691406 275.625 95.570312 348.503906 185.597656 348.503906 C 275.625 348.503906 348.503906 275.625 348.503906 185.597656 C 348.503906 95.570312 275.089844 22.691406 185.597656 22.691406 Z M 185.597656 22.691406 '
+              fill-opacity='1'
+              fill-rule='nonzero'
+            ></path>
+          </g>
+          <g id='inner-icon' transform='translate(85, 75)'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 512 512'
+              id='IconChangeColor'
+              height='199.8'
+              width='199.8'
+            >
+              <rect
+                width='32'
+                height='192'
+                x='16'
+                y='160'
+                className='fill-primary'
+                // className='ci-primary'
+              ></rect>
+              <rect
+                width='32'
+                height='192'
+                x='376'
+                y='160'
+                className='fill-primary'
+                // className='ci-primary'
+              ></rect>
+              <rect
+                width='32'
+                height='328'
+                x='104'
+                y='88'
+                className='fill-primary'
+                // className='ci-primary'
+              ></rect>
+              <rect
+                width='32'
+                height='320'
+                x='288'
+                y='96'
+                className='fill-primary'
+                // className='ci-primary'
+              ></rect>
+              <rect
+                width='32'
+                height='320'
+                x='464'
+                y='96'
+                className='fill-primary'
+                // className='ci-primary'
+              ></rect>
+              <rect
+                width='32'
+                height='480'
+                x='192'
+                y='16'
+                className='fill-primary'
+                // className='ci-primary'
+              ></rect>
+            </svg>
+          </g>
+        </svg>
+      </a>
+    </div>
+  );
+};
+
+export default SocialLinks;
