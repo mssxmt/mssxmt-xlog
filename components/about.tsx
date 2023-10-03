0561c72c0f93a4ef7f87ae7cf056cf1008b61b1d
mssxmt <orela.rec@gmail.com>
2023-10-04 00:34:45 +0900

    logoの読み込みがアレだったので別コンポーネントへ

--- components/about.tsx
+++ components/about.tsx
@@ -1,4 +1,5 @@
 import { bt } from '../styles/fonts';
+import AboutLogo from './aboutLogo';
 import Avatar from './avatar';
 
 type Props = {
@@ -8,16 +9,7 @@ type Props = {
 const About = ({ name, picture }: Props) => {
   return (
     <section className='lg:pt-[256px] pt-[188px] flex flex-col justify-center items-center align-items-center lg:gap-y-9 text-neutral-content pb-10'>
-      <h2
-        className={`animate-text-pop-up-top lg:text-[22rem] text-[5rem] leading-[0.5em] ${bt.className} text-center	`}
-      >
-        About
-      </h2>
-      <h2
-        className={`animate-text-pop-up-top lg:text-[22rem] text-[5rem] leading-[0.5em] ${bt.className} text-center	`}
-      >
-        Us
-      </h2>
+      <AboutLogo />
       <Avatar name={name} picture={picture} />
     </section>
   );
---
+++ components/aboutLogo.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { bt } from '../styles/fonts';
+
+const AboutLogo = () => {
+  return (
+    <>
+      <h2
+        className={`animate-text-pop-up-top lg:text-[22rem] text-[5rem] leading-[0.5em] ${bt.className} text-center	`}
+      >
+        About
+      </h2>
+      <h2
+        className={`animate-text-pop-up-top lg:text-[22rem] text-[5rem] leading-[0.5em] ${bt.className} text-center	`}
+      >
+        Us
+      </h2>
+    </>
+  );
+};
+export default AboutLogo;
