2c25ae5cbc50a6e6ce3e73cb1121748cc48adbc8
mssxmt <orela.rec@gmail.com>
2023-11-07 21:43:49 +0900

    リファラテスト

---
+++ app/x/page.tsx
@@ -0,0 +1,15 @@
+export default async function Page() {
+  return (
+    <div className='hero-overlay bg-opacity-30'>
+      <div className='text-center text-neutral-content'>
+        <a
+          href='https://localhost:3000/offices/126'
+          target='_blank'
+          rel='noopener '
+        >
+          リファラテスト
+        </a>
+      </div>
+    </div>
+  );
+}
